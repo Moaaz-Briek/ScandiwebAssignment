2883115ba632e1013131672daa278991328e785f
moaaz97 <moaaz.briek97@gmail.com>
2023-02-17 14:31:47 +0200

    Edit validator to check the type first before anything.

--- backend/ProductValidatorController.php
+++ backend/ProductValidatorController.php
@@ -2,6 +2,7 @@
 
 class ProductValidatorController
 {
+    public string $error = '';
     private array $ProductIdentifier =
         [
             'dvd' => 'DvdValidator',
@@ -11,30 +12,34 @@ class ProductValidatorController
 
     function __construct(array $inputs)
     {
-        $ProductValidator = $this->ProductIdentifier[$inputs['type']];
-        $this->Validate(new $ProductValidator($inputs));
+        $type = $inputs['type'];
+        $keys = array_keys($this->ProductIdentifier);
+        if(in_array($type, $keys)) {
+            $ProductValidator = $this->ProductIdentifier[$type];
+            $this->Validate(new $ProductValidator($inputs));
+        } else {
+            $this->error .= 'Invalid type, Please select a valid type.<br>';
+            return response(array('status' => 'failed', 'error' => $this->error));
+        }
     }
 
     public function Validate(ValidationInterface $validate)
     {
-        $error = '';
         //Get ProductModel Validation Function Type
         if($validate->validateSKU())
-            $error .= 'Invalid SKU or already exists <br>';
+            $this->error .= 'Invalid SKU or already exists <br>';
         if($validate->validateName())
-            $error .= 'Invalid name <br>';
+            $this->error .= 'Invalid name <br>';
         if($validate->validatePrice())
-            $error .= 'Invalid price <br>';
-        if($validate->validateType())
-            $error .= 'Invalid type <br>';
+            $this->error .= 'Invalid price <br>';
         if(!$validate->validateAttributes())
-            $error .= 'Invalid attributes <br>';
-        if($error == null)
+            $this->error .= 'Invalid attributes <br>';
+        if($this->error == null)
         {
             $validate->saveProduct();
             return response(array('status' => 'success'));
         }
-        return response(array('status' => 'failed', 'error' => $error));
+        return response(array('status' => 'failed', 'error' => $this->error));
 
     }
 };
